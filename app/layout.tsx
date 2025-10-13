985c687109ebb1f8a45c1b0e5d22918394917a72
gero <marouanetabaa@gmail.com>
2025-10-13 19:46:27 +0100

    Adding scripts on layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,14 +25,39 @@ export default function RootLayout({
       <head>
         <meta name="color-scheme" content="light" />
         {/* Google Tag Manager */}
-        <Script id="gtm-loader" strategy="afterInteractive">
+        {/* <Script id="gtm-loader" strategy="afterInteractive">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-K2DVFD25');`}
-        </Script>
+        </Script> */}
         {/* End Google Tag Manager */}
+        {/* Google tag (gtag.js) */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17643542041"
+          strategy="afterInteractive"
+        />
+
+        {/* Google Ads base config */}
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17643542041');
+          `}
+        </Script>
+
+        {/* Phone conversion config */}
+        <Script id="gtag-phone" strategy="afterInteractive">
+          {`
+            gtag('config', 'AW-17643542041/U75ZCP-WpKwbEJmsjN1B', {
+              'phone_conversion_number': '03333447600'
+            });
+          `}
+        </Script>
       </head>
       <body className={`${montserrat.variable} font-sans`}>
         {/* Google Tag Manager (noscript) */}
