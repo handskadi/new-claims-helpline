dc601438bd7a1b03a3e1afecbcea18d976043d3a
Mohamed KADI <handskadi@gmail.com>
2025-10-14 16:24:55 +0100

    Add GTM tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import Script from "next/script";
 
-
 const montserrat = Montserrat({
   weight: ["400", "600", "700"],
   subsets: ["latin"],
@@ -24,23 +23,27 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="color-scheme" content="light" />
-        {/* Google Tag Manager */}
-        {/* <Script id="gtm-loader" strategy="afterInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-K2DVFD25');`}
-        </Script> */}
-        {/* End Google Tag Manager */}
-        {/* Google tag (gtag.js) */}
+
+        {/* ✅ Google Tag Manager */}
+        <Script id="gtm-loader" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];
+            w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+            var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+            j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+            f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-TJW8VRQS');`}
+        </Script>
+        {/* ✅ End Google Tag Manager */}
+
+        {/* ✅ Google tag (gtag.js) */}
         <Script
           async
           src="https://www.googletagmanager.com/gtag/js?id=AW-17643542041"
           strategy="afterInteractive"
         />
 
-        {/* Google Ads base config */}
+        {/* ✅ Google Ads base config */}
         <Script id="gtag-init" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
@@ -50,7 +53,7 @@ export default function RootLayout({
           `}
         </Script>
 
-        {/* Phone conversion config */}
+        {/* ✅ Google Ads phone conversion tracking */}
         <Script id="gtag-phone" strategy="afterInteractive">
           {`
             gtag('config', 'AW-17643542041/U75ZCP-WpKwbEJmsjN1B', {
@@ -59,17 +62,19 @@ export default function RootLayout({
           `}
         </Script>
       </head>
+
       <body className={`${montserrat.variable} font-sans`}>
-        {/* Google Tag Manager (noscript) */}
+        {/* ✅ Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-K2DVFD25"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TJW8VRQS"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
           />
         </noscript>
-        {/* End Google Tag Manager (noscript) */}
+        {/* ✅ End Google Tag Manager (noscript) */}
+
         {children}
       </body>
     </html>
